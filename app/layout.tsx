9e03b25ee023633c116f3fc0d8878baefdddc525
CB <chris@project9.studio>
2024-05-27 12:04:05 -0400

    Added viewport and zoom level for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "ATL Flavors | Admin",
   description: "ATL Flavor Admin Dashboard",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
